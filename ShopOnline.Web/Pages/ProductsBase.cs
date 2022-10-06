a2bcf7c7184e8d7b11815d2ccda4d0b64ecfa6d9
asparatu <asparatu@hotmail.com>
2022-10-06 19:02:47 +0800

    Add project files.

---
+++ ShopOnline.Web/Pages/ProductsBase.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Pages
+{
+    public class ProductsBase : ComponentBase
+    {
+        [Inject]
+        public IProductService ProductService { get; set; }
+
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+
+        [Inject]
+        public IManageProductsLocalStorageService ManageProductsLocalStorageService { get; set; }
+
+        [Inject]
+        public IManageCartItemsLocalStorageService ManageCartItemsLocalStorageService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public IEnumerable<ProductDto> Products { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        protected IOrderedEnumerable<IGrouping<int, ProductDto>> GetGroupProductsByCategory()
+        {
+            return from product in Products
+                   group product by product.CategoryId into prodByCatGroup
+                   orderby prodByCatGroup.Key
+                   select prodByCatGroup;
+        }
+
+        protected string GetCategoryName(IGrouping<int, ProductDto> groupProductDtos)
+        {
+            return groupProductDtos.FirstOrDefault(pg => pg.CategoryId == groupProductDtos.Key).CategoryName;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                await ClearLocalStorage();
+
+                Products = await ManageProductsLocalStorageService.GetCollection();
+
+                var shoppingCartItems = await ManageCartItemsLocalStorageService.GetCollection(HardCoded.UserId);
+
+                var totalQty = shoppingCartItems.Sum(i => i.Qty);
+
+                ShoppingCartService.RaiseEventOnShoppingCartChanged(totalQty);
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+
+        private async Task ClearLocalStorage()
+        {
+            await ManageProductsLocalStorageService.RemoveCollection();
+            await ManageCartItemsLocalStorageService.RemoveCollection();
+        }
+    }
+}
---
+++ ShopOnline.Web/Services/ManageCartItemsLocalStorageService.cs
@@ -0,0 +1,48 @@
+﻿using Blazored.LocalStorage;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Services
+{
+    public class ManageCartItemsLocalStorageService : IManageCartItemsLocalStorageService
+    {
+        private readonly IProductService productService;
+        private readonly ILocalStorageService localStorageService;
+        private readonly IShoppingCartService shoppingCartService;
+        private const string key = "CartItemCollection";
+
+        public ManageCartItemsLocalStorageService(ILocalStorageService localStorageService, IShoppingCartService shoppingCartService)
+        {
+            this.localStorageService = localStorageService;
+            this.shoppingCartService = shoppingCartService;
+        }
+
+        public async Task<List<CartItemDto>> GetCollection(int userId)
+        {
+            return await this.localStorageService.GetItemAsync<List<CartItemDto>>(key)
+                ?? await AddCollection(userId);
+        }
+
+        public async Task RemoveCollection()
+        {
+            await this.localStorageService.RemoveItemAsync(key);
+        }
+
+        public async Task SaveCollection(List<CartItemDto> cartItemDtos)
+        {
+            await this.localStorageService.SetItemAsync(key, cartItemDtos);
+        }
+
+        private async Task<List<CartItemDto>> AddCollection(int userId)
+        {
+            var shoppingCartCollection = await this.shoppingCartService.GetItems(userId);
+
+            if (shoppingCartService != null)
+            {
+                await this.localStorageService.SetItemAsync(key, shoppingCartCollection);
+            }
+
+            return shoppingCartCollection;
+        }
+    }
+}
